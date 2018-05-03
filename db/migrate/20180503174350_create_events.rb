343f9272c291b4e322b9a61bb813548c4ba633fe
kevinmainaud <kevin.mainaud@gmail.com>
2018-05-04 00:34:01 +0200

    Github 1ère version

--- db/migrate/20180503174350_create_events.rb
+++ db/migrate/20180503174350_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :name
-      t.content :description
+      t.text :description
       t.datetime :date
       t.belongs_to :usercreator
 
